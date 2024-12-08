b57d89fcdcf55c9317eda25e06640a4e30ebedb6
Gaurav Laud <gaurav.laud@innovapptive.com>
2024-12-08 20:15:36 +0530

    Resticted Theme and Screen orientation

--- app/src/main/java/com/example/interviewcontent/CalanderActivity.kt
+++ app/src/main/java/com/example/interviewcontent/CalanderActivity.kt
@@ -1,7 +1,9 @@
 package com.example.interviewcontent
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -22,6 +24,8 @@ class CalanderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val repository = CalanderRepository(DailyTaskDatabase.invoke(this))
